bac9985acd1eadc4ee25cdfca89c427499308adb
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-01-30 09:54:56 +0100

    Creating a git repo for Make

---
+++ first/c.cpp
@@ -0,0 +1,10 @@
+//A simple c++ program
+#include <iostream>
+#include <stdio.h>
+int main(void)
+{
+  using namespace std;
+  // printf("This must be displayed, if the program was successfully compiled.");
+  cout << "Hello World"<< endl << "Why are you not displaying???";
+  return 0;
+}
