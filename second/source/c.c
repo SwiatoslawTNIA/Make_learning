00af1ffd3712dc1c312bb843473b1af61f27e06d
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-02-02 11:37:34 +0100

    Cleared folders

--- second/source/c.c
+++ second/source/c.c
@@ -1,7 +1,8 @@
-#include "../headers/c.h"
+#include "c.h"
 int main(void)
 {
   fprintf(stdout, "Hello World!!!");
   fprintf(stdout, "Changed the file since the last update.");
+  fprintf(stdout, "\n");
   return 0;
 }
--- second/source/c.cc
+++ second/source/c.cc
@@ -1,4 +1,4 @@
-#include "../headers/cpp.h"
+#include "cpp.h"
 using namespace std;
 int main(void)
 {
